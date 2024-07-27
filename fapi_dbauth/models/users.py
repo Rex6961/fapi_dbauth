7125f85c967862e1dda5c3489e0ef51a04b56c77
Rex6961 <aon777ar@gmail.com>
2024-07-27 16:05:43 -0300

    Routes and customer models created.

---
+++ fapi_dbauth/models/users.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel, EmailStr
+from models.events import Event
+
+
+class User(BaseModel):
+    email: EmailStr
+    password: str
+    events: list[Event] = None
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "email": "fastapi@packt.com",
+                "username": "strong!!!",
+                "events": [],
+            }
+        }
+    }
+
+class UserSingIn(BaseModel):
+    email: EmailStr
+    password: str
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "email": "fastapi@packt.com",
+                "password": "strong!!!"
+            }
+        }
+    }
