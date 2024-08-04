3a16651f54627e606146b3dc400e2d8ecbf0e61e
Rex6961 <aon777ar@gmail.com>
2024-08-04 02:59:27 -0300

    [Feature] Incorporate a SQL database and implement CRUD operations

--- fapi_dbauth/main.py
+++ fapi_dbauth/main.py
@@ -1,12 +1,28 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from database.connection import conn
+
 from routes.users import user_router
 from routes.events import event_router
+
 import uvicorn
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    conn()
+    yield
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
+
+
+# Todo app.on_event ar app.get
+@app.get("/")
+async def home():
+    return RedirectResponse(url="/event/")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
--- fapi_dbauth/models/events.py
+++ fapi_dbauth/models/events.py
@@ -1,12 +1,12 @@
-from pydantic import BaseModel
+from sqlmodel import JSON, SQLModel, Field, Column
 
 
-class Event(BaseModel):
-    id: int
+class Event(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
     title: str
     image: str
     description: str
-    tags: list[str]
+    tags: list[str] = Field(sa_column=Column(JSON))
     location: str
 
     model_config = {
@@ -16,8 +16,27 @@ class Event(BaseModel):
                 "title": "FastAPI Book Launch",
                 "image": "https://linktomyimage.com/image.png",
                 "description": "We will be discussing the contents \
-of the FastAPI book in this event. Ensure to \
-come with your own copy to win gifts!",
+of the FastAPI book in this event. Ensure to come with your own copy to win gifts!",
+                "tags": ["python", "fastapi", "book", "launch"],
+                "location": "Google Meet"
+            }
+        }
+    }
+
+class EventUpdate(SQLModel):
+    title: str = None
+    image: str = None
+    description: str = None
+    tags: str = None
+    location: str = None
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "FastAPI Book Launch",
+                "image": "https://linkmyimage.com/image.png",
+                "description": "We will be discussing the contents \
+of the FastAPI book in this event. Ensure to come with your own copy to win gifts!",
                 "tags": ["python", "fastapi", "book", "launch"],
                 "location": "Google Meet"
             }
